87a06471726da2efe9e309815752f41856d80127
chem <99308995+chem4111@users.noreply.github.com>
2025-03-17 11:48:04 +0800

    Update TwoColor.py

--- TwoColor.py
+++ TwoColor.py
@@ -2,8 +2,9 @@ import requests
 from collections import Counter
 from datetime import datetime
 import random
+
 PUSH_PLUS_TOKEN = "your_pushplus_token"  # 请替换为你的 PushPlus token
-PUSH_PLUS_USER = "your_pushplus_topic"  # 请替换为你的 PushPlus topic
+PUSH_PLUS_USER = "your_pushplus_topic"    # 请替换为你的 PushPlus topic
 
 class Ssq:
     def __init__(self, code, red, blue):
@@ -12,7 +13,8 @@ class Ssq:
         self.blue = blue
 
 def get_recent_data():
-    url = "http://www.cwl.gov.cn/cwl_admin/front/cwlkj/search/kjxx/findDrawNotice?name=ssq&issueCount=&issueStart=&issueEnd=&dayStart=&dayEnd=&pageNo=1&pageSize=30&week=&systemType=PC"
+    url = ("http://www.cwl.gov.cn/cwl_admin/front/cwlkj/search/kjxx/"
+           "findDrawNotice?name=ssq&issueCount=&issueStart=&issueEnd=&dayStart=&dayEnd=&pageNo=1&pageSize=30&week=&systemType=PC")
     try:
         res = requests.get(url)
         res.raise_for_status()
@@ -34,8 +36,8 @@ def calculate_probabilities(kjList):
     total_records = len(kjList)
 
     for record in kjList:
-        red_counter.update(record.red。split(','))
-        blue_counter.update(record.blue。split(','))
+        red_counter.update(record.red.split(','))
+        blue_counter.update(record.blue.split(','))
 
     red_probabilities = {num: count / total_records for num, count in red_counter.items()}
     blue_probabilities = {num: count / total_records for num, count in blue_counter.items()}
@@ -45,7 +47,6 @@ def calculate_probabilities(kjList):
 def send_notice(content, token, topic):
     title = "双色球预测"
     url = f"http://www.pushplus.plus/send?token={token}&title={title}&content={content}&template=html&topic={topic}"
-    
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -53,7 +54,7 @@ def send_notice(content, token, topic):
         print(f"发送通知失败: {e}")
 
 # 获取当前日期
-current_date = datetime.当前()
+current_date = datetime.now()
 
 # 获取最新一期的数据
 kjList = get_recent_data()
@@ -65,12 +66,12 @@ else:
     predicted_data = []
     for i in range(5):
         # 生成红球
-        red_balls = random.sample(range(1， 34)， 6)
+        red_balls = random.sample(range(1, 34), 6)
         red_balls.sort()
-        
+
         # 生成蓝球
-        blue_ball = random.randint(1， 16)
-        
+        blue_ball = random.randint(1, 16)
+
         predicted_data.append({"红球": red_balls, "蓝球": blue_ball})
 
     # 打印预测的下一期五组号码
@@ -78,12 +79,12 @@ else:
     predicted_content = ""
     for i, data in enumerate(predicted_data, 1):
         predicted_content += f"预测第 {i} 组号码:\n"
-        predicted_content += "红球: " + ", "。join(map(str, data["红球"])) + "\n"
+        predicted_content += "红球: " + ", ".join(map(str, data["红球"])) + "\n"
         predicted_content += "蓝球: " + str(data["蓝球"]) + "\n"
-
+        
         # 打印每组号码
         print(f"预测第 {i} 组号码:")
-        print("红球:"， ", "。join(map(str, data["红球"])))
+        print("红球: " + ", ".join(map(str, data["红球"])))
         print("蓝球:", data["蓝球"])
         print()
 
@@ -119,8 +120,8 @@ else:
 
     # 获取最新一期的开奖信息
     latest_record = kjList[0]
-    latest_content = f"\n最新一期的开奖信息：\n期数: {latest_record.code}, 红球: {latest_record.red}, 蓝球: {latest_record.blue}\n"
+    latest_content = (f"\n最新一期的开奖信息：\n期数: {latest_record.code}, 红球: {latest_record.red}, "
+                      f"蓝球: {latest_record.blue}\n")
 
     # 发送 PushPlus 通知
-
     send_notice(predicted_content + latest_content, PUSH_PLUS_TOKEN, PUSH_PLUS_USER)
