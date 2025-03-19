3a2d2203f8346dd4b1e4fe5b2bf58782bb62d595
chem <99308995+chem4111@users.noreply.github.com>
2025-03-19 15:01:25 +0800

    Update cache_clean.sh

--- cache_clean.sh
+++ cache_clean.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-# 获取总内存
+# 从 /proc/meminfo 中获取总内存
 total_memory=$(grep 'MemTotal:' /proc/meminfo | awk '{print $2}')
-# 获取可用内存
+# 从 /proc/meminfo 中获取可用内存
 available_memory=$(grep 'MemAvailable:' /proc/meminfo | awk '{print $2}')
 
-# 计算实际内存使用率
+# 计算真实内存使用率
 used_percentage=$(echo "scale=2; (1 - $available_memory / $total_memory) * 100" | bc)
 
-# 判断内存使用率是否大于 85%
-if (( $(echo "$used_percentage > 85" | bc -l) )); then
+# 设定清理缓存的阈值（这里设为 85%）
+threshold=85
+
+# 判断真实内存使用率是否超过阈值
+if (( $(echo "$used_percentage > $threshold" | bc -l) )); then
     # 同步数据到磁盘并清理缓存
     sync && echo 3 > /proc/sys/vm/drop_caches
-    echo "物理内存使用率达到 $used_percentage%，已清理缓存。"
+    echo "真实物理内存使用率达到 $used_percentage%，已清理缓存。"
 else
-    echo "物理内存使用率为 $used_percentage%，未达到 85%，无需清理。"
-fi    
+    echo "真实物理内存使用率为 $used_percentage%，未达到 $threshold%，无需清理。"
+fi
+    
