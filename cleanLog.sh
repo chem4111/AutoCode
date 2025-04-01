a6db87950af5ff8eb89e2cb4ad248e47f1bf0c91
chem <99308995+chem4111@users.noreply.github.com>
2025-04-01 11:30:05 +0800

    Update cleanLog.sh

--- cleanLog.sh
+++ cleanLog.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 # -- coding: utf-8 --
 # -------------------------------
 # @Author : https://github.com/chem4111/AutoCode/
@@ -7,10 +7,35 @@
 # cron "30 0 * * *" script-path=xxx.py,tag=匹配cron用
 # const $ = new Env('青龙日志清理')
 
-#!/bin/bash
+# 定义日志目录
+LOG_DIR="../log"
+
 echo "开始清理日志"
 pwd
-# ls ../log
-find ../log -mtime +10 -name "*.log"
-find ../log -mtime +10 -name "*.log" -exec rm -rf {} \;
-echo "清理日志完成"
+
+# 检查日志目录是否存在
+if [ ! -d "$LOG_DIR" ]; then
+    echo "错误：日志目录 $LOG_DIR 不存在。"
+    exit 1
+fi
+
+# 查找要删除的日志文件
+LOG_FILES=$(find "$LOG_DIR" -name "*.log")
+
+if [ -z "$LOG_FILES" ]; then
+    echo "没有找到日志文件，无需清理。"
+else
+    # 输出要删除的日志文件列表
+    echo "即将删除以下日志文件："
+    echo "$LOG_FILES"
+
+    # 删除日志文件
+    if find "$LOG_DIR" -name "*.log" -exec rm -rf {} \; ; then
+        echo "日志文件删除成功。"
+    else
+        echo "错误：删除日志文件时出现问题。"
+        exit 1
+    fi
+fi
+
+echo "清理日志完成"    
