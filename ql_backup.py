344792789ee2ba68b23b67ebf26a075a77ffa99f
chem <99308995+chem4111@users.noreply.github.com>
2025-09-23 10:30:56 +0800

    Update ql_backup.py

--- ql_backup.py
+++ ql_backup.py
@@ -1,3 +1,12 @@
+#!/usr/bin/python3
+# -- coding: utf-8 --
+# -------------------------------
+# @Author : https://github.com/chem4111/AutoCode/
+# @Time : 2025/9/23 13:23
+# -------------------------------
+# cron "30 8 * * *" script-path=xxx.py,tag=匹配cron用
+# const $ = new Env('青龙环境变量备份')
+
 # ql_backup.py
 import requests
 import json
