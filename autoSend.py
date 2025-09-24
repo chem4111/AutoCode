7517bbf5ac10689decc4f03f3de23be31d42129e
chem <99308995+chem4111@users.noreply.github.com>
2025-09-24 10:59:34 +0800

    Update autoSend.py

--- autoSend.py
+++ autoSend.py
@@ -4,7 +4,7 @@
 # @Author : https://github.com/chem4111/AutoCode/
 # @Time : 2025/3/27 13:23
 # -------------------------------
-# cron "* * * 8 *" script-path=xxx.py,tag=匹配cron用
+# cron "0 0 1 8 *" script-path=xxx.py,tag=匹配cron用
 # const $ = new Env('自动匹配推送json')
 
 import json
